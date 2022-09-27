61240ed57a0f5d7b4caddb253c9980574da74cd7
3aly <thebigali1998@gmail.com>
2022-09-27 02:31:16 +0200

    everything is func comp

--- src/App.js
+++ src/App.js
@@ -1,44 +1,88 @@
 import logo from "./logo.svg";
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import "./App.css";
 import { CardList } from "./components/card-list/card-list.component.jsx";
 import { Search } from "./components/search/search.component";
 
-class App extends Component {
-  constructor() {
-    super();
+export default function App() {
+  const [searchValue, setSearchValue] = useState("");
 
-    this.state = {
-      monsters: [],
-      search: "",
-    };
-  }
+  const [monsters, setMonsters] = useState([]);
 
-  componentDidMount() {
+  const [filterdMonsters, setFilterdMonsters] = useState(monsters);
+
+  const handelChange = (e) => {
+    setSearchValue(e.target.value.toLowerCase());
+  };
+
+  useEffect(() => {
     fetch("https://reqres.in/api/users/")
       .then((response) => response.json())
-      .then((users) => this.setState({ monsters: users.data }));
-  }
+      .then((users) => setMonsters(users.data));
+  }, []);
 
-  handelChange = (e) => {
-    this.setState({ search: e.target.value });
-  };
-  render() {
-    const { monsters, search } = this.state;
-    const filterdMonsters = monsters.filter((monster) =>
-      monster.first_name.toLowerCase().includes(search.toLowerCase())
+  useEffect(() => {
+    setFilterdMonsters(
+      monsters.filter(
+        (monster) =>
+          monster.first_name
+            .toLowerCase()
+            .includes(searchValue.toLowerCase()) ||
+          monster.last_name.toLowerCase().includes(searchValue.toLowerCase())
+      )
     );
-    return (
-      <div className="App">
-        <h1>Sims City</h1>
-        <Search
-          placeholder={"Search.."}
-          handleChange={this.handelChange}
-        ></Search>
-        <CardList monsters={filterdMonsters}></CardList>
-      </div>
-    );
-  }
+  }, [monsters, searchValue]);
+
+  // const filterdMonsters = monsters.filter(
+  //   (monster) =>
+  //     monster.first_name.toLowerCase().includes(searchValue.toLowerCase()) ||
+  //     monster.last_name.toLowerCase().includes(searchValue.toLowerCase())
+  // );
+
+  return (
+    <div className="App">
+      <h1>Sims City</h1>
+      <Search placeholder={"Search.."} handleChange={handelChange}></Search>
+      <CardList monsters={filterdMonsters}></CardList>
+    </div>
+  );
 }
 
-export default App;
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       monsters: [],
+//       search: "",
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch("https://reqres.in/api/users/")
+//       .then((response) => response.json())
+//       .then((users) => this.setState({ monsters: users.data }));
+//   }
+
+//   handelChange = (e) => {
+//     this.setState({ search: e.target.value });
+//   };
+//   render() {
+//     const { monsters, search } = this.state;
+//     const filterdMonsters = monsters.filter((monster) =>
+//       monster.first_name.toLowerCase().includes(search.toLowerCase())
+//     );
+//     return (
+//       <div className="App">
+//         <h1>Sims City</h1>
+//         <Search
+//           placeholder={"Search.."}
+//           handleChange={this.handelChange}
+//         ></Search>
+//         <CardList monsters={filterdMonsters}></CardList>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
